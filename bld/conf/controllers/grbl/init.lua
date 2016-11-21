4eeaa2fa1565642f4f04609dd6f0567015fce416
bhgv <bhgv.empire@gmail.com>
2016-11-21 17:56:53 +0200

    auto set to correct mode (grbl 0 should be 15)

--- bld/conf/controllers/grbl/init.lua
+++ bld/conf/controllers/grbl/init.lua
@@ -244,6 +244,11 @@ return {
       
       if _stat_mode then
         status_mode = tonumber(_stat_mode)
+        if status_mode ~= 15 then
+          Sender:newcmd("SINGLE")
+          Sender:newcmd("$10=15 (mode should be 15 for correct work)")
+          status_mode = 15
+        end
         status_mask = status_mask_choises[status_mode]
         read_timeout = read_timeout_choises[status_mode]
       end
